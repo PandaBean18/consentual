b86be2ee7a5671fc279812d435c5bb0aa3b56f40
Raghav Bahukhandi <raghapb34n@gmail.com>
2024-07-27 20:35:25 +0530

    added a bottom navbar

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,4 +1,5 @@
 import "package:flutter/material.dart";
+import "package:flutter/widgets.dart";
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -14,23 +15,53 @@ class _HomePageState extends State<HomePage> {
     SafeArea(
       child: Scaffold(
         appBar: AppBar(
-          title: const Text("Consentual", style: TextStyle(fontFamily: 'Jost', fontSize: 35)),
+          title: const Text("App Name", style: TextStyle(fontFamily: 'Jost', fontSize: 35)),
           backgroundColor: theme.colorScheme.secondary,
           foregroundColor: theme.colorScheme.tertiary,
-          actions: const [
+          actions: [
+            // need to update this to use the propfile image from google oauth
             Icon(
               Icons.account_circle_outlined,
               size: 44,
+              color: theme.colorScheme.primary,
             ), 
-            SizedBox(
+            const SizedBox(
               width: 10,
             )
           ],
         ),
 
-        body: Container(
-          color: theme.colorScheme.primary,
+        body: Stack(
+          children: [
+            Container(
+              color: theme.colorScheme.primary,
+            ),
+            Align(
+              alignment: Alignment.bottomCenter,
+              child: Container(
+                color: theme.colorScheme.secondary,
+                height: 60,
+                child:  Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceAround,
+                  children: [
+                    Icon(
+                      Icons.home, 
+                      size: 44,
+                      color: theme.colorScheme.primary,
+                    ), 
+                    Icon(
+                      Icons.file_present,
+                      size: 44, 
+                      color: theme.colorScheme.primary
+                    )
+                  ],
+                ),
+              )
+            )
+          ],
         )
+
+        
       )
     );
   }
