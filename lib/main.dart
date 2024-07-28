6f2ba6ac2ab2bcf5037b6405302014595d90f0d1
Raghav Bahukhandi <raghapb34n@gmail.com>
2024-07-28 14:42:05 +0530

    added test code for google sign in

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,34 @@
+import 'package:google_sign_in/google_sign_in.dart';
 import "package:flutter/material.dart";
 import "./pages/home_page.dart";
 
+const List<String> scopes = <String>[
+  'email',
+  'https://www.googleapis.com/auth/contacts.readonly',
+];
+
+GoogleSignIn _googleSignIn = GoogleSignIn(
+  // Optional clientId
+  // clientId: 'your-client_id.apps.googleusercontent.com',
+  scopes: scopes,
+);
 void main() {
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
+  Future<void> _handleSignIn() async {
+    try {
+      await _googleSignIn.signIn();
+    } catch (error) {
+      print('Error: ');
+      print(error);
+    }
+  }
   @override
   Widget build(BuildContext context) {
-    
+   _handleSignIn();
     return MaterialApp(
       title: "Consentual",
       theme: ThemeData(
